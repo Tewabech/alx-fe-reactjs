f8ddf930722434313e4514ccefa7aaa79bf9d9b2
Tewabech Mengistu <etse30@gmail.com>
2025-09-05 23:52:17 +0300

    react with header footer

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,5 +1,8 @@
 
 import WelcomeMessage from './components/WelcomeMessage'
+import Header from './Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -21,6 +24,11 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
+      <div>
+      <Header />
+      <MainContent />
+      <Footer />
+    </div>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
