75d76aaeb04aaed18fc5b6834bc98a1414f9d63b
Tewabech Mengistu <etse30@gmail.com>
2025-09-06 11:06:28 +0300

    user profile input

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,6 +3,7 @@ import WelcomeMessage from './components/WelcomeMessage'
 import Header from './Header';
 import MainContent from './MainContent';
 import Footer from './Footer';
+import UserProfile from './components/UserProfile';
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -16,6 +17,18 @@ function App() {
     <div>
       <WelcomeMessage />
       <Header />
+      <div>
+      <UserProfile 
+        name="Alice" 
+        age="25" 
+        bio="Loves hiking and photography" 
+      />
+      <UserProfile 
+        name="John" 
+        age="30" 
+        bio="Software engineer who enjoys coding and coffee" 
+      />
+    </div>
       <MainContent />
       <Footer />
     </div>
