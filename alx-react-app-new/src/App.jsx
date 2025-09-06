c921c472dabb57bf45b05403cff5f1d3cfadee5c
Tewabech Mengistu <etse30@gmail.com>
2025-09-06 22:57:25 +0300

    increament decrement

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -4,6 +4,7 @@ import Header from './Header';
 import MainContent from './MainContent';
 import Footer from './Footer';
 import UserProfile from './components/UserProfile';
+import Counter from './components/Counter';
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -53,6 +54,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <div>
+        <Counter />
+      </div>
     </>
   )
 }
